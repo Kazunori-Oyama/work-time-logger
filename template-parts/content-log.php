f853fc9b64eea8be30371e6644f26b2c9f562642
大山和徳 <kazunori-oyama@tonichi-printing.co.jp>
2019-08-19 08:42:38 +0900

    change table structures

--- template-parts/content-log.php
+++ template-parts/content-log.php
@@ -9,43 +9,30 @@
 
 ?>
 <?php $author_name = get_the_author();?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		
 		<tr>
 		<td class="date">
 		<?php $date = get_the_date('Ymj')?>
 		<?php the_time('Y/n/j'); ?>
 			<?php echo '<p><a href="'.get_home_url().'/?post_type=post&m='.$date.'" style="display:inline; text-align:right;">';?>
-			
+			<?php the_field('startTime');?>
+			<span>~</span>
+			<?php the_field('endTime');?>
 			<?php echo '<i class="far fa-file-alt" title="この日のレポートを見る"></i></a>';?>
 		</td>
-		<td class="startTime"><?php the_field('startTime');?></td>
-		<td class="endTime"><?php the_field('endTime');?></td>
-		<td>実働時間：<span class="workTime"><?php the_field('worktime');?></span>min
-		<br>予想時間：<?php the_field('preestimate');?>min</td>
+		<!-- <td class="startTime"></td>
+		<td class="endTime"></td> -->
+		<td>実働時間：<span class="workTime"><?php the_field('worktime');?></span>min</td>
+		<td>予想時間：<?php the_field('preestimate');?>min</td>
 		<td class="in-charge"><a href="<?php echo home_url();?>/?post_type=logs&author=<?php echo the_author_meta('ID');?>"><?php echo $author_name;?></a></td>
 		<td class="project"><?php echo get_the_term_list($post->ID,'project');?></td>
 		<td class="step"><?php echo get_the_term_list($post->ID,'step');?></td>
-		<td class=""><?php the_field('todaysTask');?></td>
-		<td class=""><?php the_field('comment');?></td>
-		<td class=""><?php the_field('progress');?>%</td>
-		<td></td>
-		<td class=""><?php wp_bootstrap_starter_entry_footer(); ?></td>
+		<td colspan="4" class="tasks"><?php the_field('todaysTask');?></td>
+		<td class="memos"><?php the_field('comment');?></td>
+		<!-- <td class=""><?php the_field('progress');?>%</td> -->
+		<!-- <td class=""><?php wp_bootstrap_starter_entry_footer(); ?></td> -->
 		</tr>
-		<tr>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		<td></td>
-		</tr>
+		
 		
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
@@ -68,4 +55,3 @@
 	
 		
 	
-</article><!-- #post-## -->
